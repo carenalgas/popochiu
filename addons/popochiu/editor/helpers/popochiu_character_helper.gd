912a68143b17b1612ff1211412f7a50909890c46
Paolo Pustorino <paolo.pustorino@sparkfabrik.com>
2023-07-17 09:31:01 +0200

    refs #37: Characters creation have been refactored.

--- addons/popochiu/editor/helpers/popochiu_character_helper.gd
+++ addons/popochiu/editor/helpers/popochiu_character_helper.gd
@@ -1,48 +1,48 @@
 extends 'res://addons/popochiu/editor/helpers/popochiu_obj_base_helper.gd'
-class_name PopochiuRoomHelper
+class_name PopochiuCharacterHelper
 
-const BASE_STATE_TEMPLATE := 'res://addons/popochiu/engine/templates/room_state_template.gd'
-const BASE_SCRIPT_TEMPLATE := 'res://addons/popochiu/engine/templates/room_template.gd'
-const BASE_OBJ_PATH := 'res://addons/popochiu/engine/objects/room/popochiu_room.tscn'
+const BASE_STATE_TEMPLATE := 'res://addons/popochiu/engine/templates/character_state_template.gd'
+const BASE_SCRIPT_TEMPLATE := 'res://addons/popochiu/engine/templates/character_template.gd'
+const BASE_OBJ_PATH := 'res://addons/popochiu/engine/objects/character/popochiu_character.tscn'
 
 
 # ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ PUBLIC ░░░░
 func init(_main_dock: Panel) -> void:
 	super(_main_dock)
-	_obj_path_template = _main_dock.ROOMS_PATH + '%s/room_%s'
+	_obj_path_template = _main_dock.CHARACTERS_PATH + '%s/character_%s'
 
 
 
-func create(obj_name: String, set_as_main:bool = false) -> PopochiuRoom:
+func create(obj_name: String) -> PopochiuCharacter:
 	# TODO: Check if another Prop was created in the same PATH.
 	# TODO: Remove created files if the creation process failed.
 	_setup_name(obj_name)
 
 	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-	# Create the folder for the room
-	DirAccess.make_dir_absolute(_main_dock.ROOMS_PATH + _obj_script_name)
+	# Create the folder for the character
+	DirAccess.make_dir_absolute(_main_dock.CHARACTERS_PATH + _obj_script_name)
 	
 	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-	# Create the state Resource for the room and a script so devs can add extra
+	# Create the state Resource for the character and a script so devs can add extra
 	# properties to that state
 	var state_template: Script = load(BASE_STATE_TEMPLATE).duplicate()
 	if ResourceSaver.save(state_template, _obj_path + '_state.gd') != OK:
-		push_error('[Popochiu] Could not create room state script: %s' %_obj_name)
+		push_error('[Popochiu] Could not create character state script: %s' %_obj_name)
 		# TODO: Show feedback in the popup
 		return
 
-	var obj_resource: PopochiuRoomData = load(_obj_path + '_state.gd').new()
+	var obj_resource: PopochiuCharacterData = load(_obj_path + '_state.gd').new()
 	obj_resource.script_name = _obj_name
 	obj_resource.scene = _obj_path + '.tscn'
 	obj_resource.resource_name = _obj_name
 	
 	if ResourceSaver.save(obj_resource, _obj_path + '.tres') != OK:
-		push_error("[Popochiu] Couldn't create PopochiuRoomData for room: %s" % _obj_name)
+		push_error("[Popochiu] Couldn't create PopochiuCharacterData for character: %s" % _obj_name)
 		# TODO: Show feedback in the popup
 		return
 	
 	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-	# Create the script for the room
+	# Create the script for the character
 	var obj_script: Script = load(BASE_SCRIPT_TEMPLATE).duplicate()
 	var new_code := obj_script.source_code
 	
@@ -54,8 +54,8 @@ func create(obj_name: String, set_as_main:bool = false) -> PopochiuRoom:
 		return
 	
 	new_code = new_code.replace(
-		'room_state_template',
-		'room_%s_state' % _obj_script_name
+		'character_state_template',
+		'character_%s_state' % _obj_script_name
 	)
 	
 	new_code = new_code.replace(
@@ -72,55 +72,50 @@ func create(obj_name: String, set_as_main:bool = false) -> PopochiuRoom:
 		return
 	
 	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-	# Create the room instance
-	var obj: PopochiuRoom = load(BASE_OBJ_PATH).instantiate()
+	# Create the character instance
+	var obj: PopochiuCharacter = load(BASE_OBJ_PATH).instantiate()
 	
 	# 	The script is assigned first so that other properties will not be
 	# 	overwritten by that assignment.
 	obj.set_script(load(_obj_path + '.gd'))
 	
-	obj.name = 'Room' + _obj_name
+	obj.name = 'Character' + _obj_name
 	obj.script_name = _obj_name
+	obj.description = _obj_name.capitalize()
+	obj.cursor = Constants.CURSOR_TYPE.TALK
 	
 	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-	# Save the room scene (.tscn)
+	# Save the character scene (.tscn)
 	var packed_scene: PackedScene = PackedScene.new()
 	packed_scene.pack(obj)
 	if ResourceSaver.save(packed_scene, _obj_path + '.tscn') != OK:
-		push_error("[Popochiu] Couldn't create room: %s" % _obj_name)
+		push_error("[Popochiu] Couldn't create character: %s" % _obj_name)
 		# TODO: Show feedback in the popup
 		return
 
 	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 	# Load the scene to be returned to the calling code
 	# Instancing the created .tscn file fixes #58
-	var obj_instance: PopochiuRoom = load(_obj_path + '.tscn').instantiate()
+	var obj_instance: PopochiuCharacter = load(_obj_path + '.tscn').instantiate()
 
 
 	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-	# Add the created room to Popochiu's rooms list
+	# Add the created character to Popochiu's characters list
 	if _main_dock.add_resource_to_popochiu(
-		'rooms', ResourceLoader.load(_obj_path + '.tres')
+		'characters', ResourceLoader.load(_obj_path + '.tres')
 	) != OK:
-		push_error("[Popochiu] Couldn't add the created room to Popochiu: %s" %	_obj_name)
+		push_error("[Popochiu] Couldn't add the created character to Popochiu: %s" % _obj_name)
 		# TODO: Show feedback in the popup
 		return
 	
 	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-	# Add the room to the R singleton
+	# Add the character to the C singleton
 	PopochiuResources.update_autoloads(true)
 	
 	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-	# Update the list of rooms in the dock
+	# Update the list of characters in the dock
 	var row := (_main_dock as MainDock).add_to_list(
-		Constants.Types.ROOM, _obj_name
+		Constants.Types.CHARACTER, _obj_name
 	)
 	
-	# Establecer como la escena principal
-	# Changed _set_as_main_check.pressed to _set_as_main_check.button_pressed
-	# in order to fix #56
-	if set_as_main:
-		_main_dock.set_main_scene(obj_resource.scene)
-		row.is_main = true # So the Heart icon shows
-
 	return obj_instance
--- addons/popochiu/editor/popups/create_character/create_character.gd
+++ addons/popochiu/editor/popups/create_character/create_character.gd
@@ -9,21 +9,11 @@
 @tool
 extends 'res://addons/popochiu/editor/popups/creation_popup.gd'
 
-const CHARACTER_STATE_TEMPLATE :=\
-'res://addons/popochiu/engine/templates/character_state_template.gd'
-const CHARACTER_SCRIPT_TEMPLATE :=\
-'res://addons/popochiu/engine/templates/character_template.gd'
-const CHARACTER_SCENE :=\
-'res://addons/popochiu/engine/objects/character/popochiu_character.tscn'
-const Constants := preload('res://addons/popochiu/popochiu_resources.gd')
-const PopochiuDock :=\
-preload('res://addons/popochiu/editor/main_dock/popochiu_dock.gd')
+const Helper := preload("res://addons/popochiu/editor/helpers/popochiu_character_helper.gd")
+const PopochiuDock := preload('res://addons/popochiu/editor/main_dock/popochiu_dock.gd')
 
 var _new_character_name := ''
-var _new_character_path := ''
-var _character_path_template := ''
-var _pascal_name := ''
-
+var _helper: PopochiuCharacterHelper
 
 # ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ GODOT ░░░░
 func _ready() -> void:
@@ -37,130 +27,19 @@ func _create() -> void:
 		_error_feedback.show()
 		return
 	
-	# TODO: Check that there is not a character in the same PATH.
-	# TODO: Delete created files if creation is not complete.
-	
 	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-	# Create the folder for the character
-	DirAccess.make_dir_absolute(
-		(_main_dock as PopochiuDock).CHARACTERS_PATH + _new_character_name
-	)
-	
-	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-	# Create the state Resource for the character and a script so devs
-	# can add extra properties to that state
-	var state_template: Script = load(CHARACTER_STATE_TEMPLATE).duplicate()
-	if ResourceSaver.save(
-		state_template, _new_character_path + '_state.gd'
-	) != OK:
-		push_error('[Popochiu] Could not create character state script: %s' %\
-		_new_character_name)
-		# TODO: Show feedback in the popup
-		return
-	
-	var character_resource: PopochiuCharacterData =\
-	load(_new_character_path + '_state.gd').new()
-	character_resource.script_name = _pascal_name
-	character_resource.scene = _new_character_path + '.tscn'
-	character_resource.resource_name = _pascal_name
-	
-	if ResourceSaver.save(
-		character_resource, _new_character_path + '.tres'
-	) != OK:
-		push_error(
-			"[Popochiu] Couldn't create PopochiuCharacterData for: %s" %\
-			_new_character_name
-		)
-		# TODO: Show feedback in the popup
-		return
+	# Setup the prop helper and use it to create the prop
+	_helper = Helper.new()
+	_helper.init(_main_dock)
 
-	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-	# Create the script for the character
-	var character_script: Script = load(CHARACTER_SCRIPT_TEMPLATE).duplicate()
-	var new_code := character_script.source_code
-	
-	character_script.source_code = ''
-	
-	if ResourceSaver.save(character_script, _new_character_path + '.gd') != OK:
-		push_error(
-			'[Popochiu] Could not create script: %s.gd' % _new_character_name
-		)
-		# TODO: Show feedback in the popup
-		return
-	
-	new_code = new_code.replace(
-		'character_state_template',
-		'character_%s_state' % _new_character_name
-	)
-	
-	new_code = new_code.replace(
-		'Data = null',
-		"Data = load('%s.tres')" % _new_character_path
-	)
-	
-	character_script = load(_new_character_path + '.gd')
-	character_script.source_code = new_code
-	
-	if ResourceSaver.save(character_script, _new_character_path + '.gd') != OK:
-		push_error(
-			'[Popochiu] Could not update script: %s.gd' % _new_character_name
-		)
-		# TODO: Show feedback in the popup
-		return
-
-	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-	# Create the Character instance
-	var new_character: PopochiuCharacter = load(CHARACTER_SCENE).instantiate()
-	# 	The script is assigned first so that other properties will not be
-	# 	overwritten by that assignment.
-	new_character.set_script(load(_new_character_path + '.gd'))
-	
-	new_character.name = 'Character' + _pascal_name
-	new_character.script_name = _pascal_name
-	new_character.description = _new_character_name.capitalize()
-	new_character.cursor = Constants.CURSOR_TYPE.TALK
-	
-	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-	# Save the character scene (.tscn)
-	var new_character_packed_scene: PackedScene = PackedScene.new()
-	new_character_packed_scene.pack(new_character)
-	if ResourceSaver.save(
-		new_character_packed_scene, _new_character_path + '.tscn'
-	) != OK:
-		push_error(
-			"[Popochiu] Couldn't create character: %s.tscn" % _new_character_name
-		)
-		# TODO: Show feedback in the popup
-		return
-	
-	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-	# Add the created character to Popochiu's characters list
-	if _main_dock.add_resource_to_popochiu(
-		'characters', ResourceLoader.load(_new_character_path + '.tres')
-	) != OK:
-		push_error(
-			"[Popochiu] Couldn't add the created character to Popochiu: %s" %\
-			_new_character_name
-		)
-		# TODO: Show feedback in the popup
-		return
-	
-	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-	# Add the character to the C singleton
-	PopochiuResources.update_autoloads(true)
-	
-	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-	# Update the list of characters in the dock
-	(_main_dock as PopochiuDock).add_to_list(
-		Constants.Types.CHARACTER, _pascal_name
-	)
+	var character_scene = _helper.create(_new_character_name)
 	
 	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 	# Open the scene in the editor
 	await get_tree().create_timer(0.1).timeout
 	
-	_main_dock.ei.select_file(_new_character_path + '.tscn')
-	_main_dock.ei.open_scene_from_path(_new_character_path + '.tscn')
+	_main_dock.ei.select_file(character_scene.scene_file_path)
+	_main_dock.ei.open_scene_from_path(character_scene.scene_file_path)
 	
 	# ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 	# That's all!!!
@@ -169,7 +48,6 @@ func _create() -> void:
 
 func _clear_fields() -> void:
 	_new_character_name = ''
-	_new_character_path = ''
 
 
 # ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ SET & GET ░░░░
@@ -178,9 +56,6 @@ func set_main_dock(node: Panel) -> void:
 	
 	if not _main_dock: return
 	
-	# res://popochiu/characters
-	_character_path_template = _main_dock.CHARACTERS_PATH + '%s/character_%s'
-
 
 # ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ PRIVATE ░░░░
 func _update_name(new_text: String) -> void:
@@ -188,9 +63,6 @@ func _update_name(new_text: String) -> void:
 
 	if _name:
 		_new_character_name = _name.to_snake_case()
-		_pascal_name = _name
-		_new_character_path = _character_path_template %\
-		[_new_character_name, _new_character_name]
 
 		_info.text = (
 			'In [b]%s[/b] the following files will be created:\
