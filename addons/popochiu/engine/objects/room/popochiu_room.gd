81870b35ddfc0bdf697eaa78a8f3243cd1001bd7
Juan Ferrari <juannferrari@gmail.com>
2023-11-10 01:28:02 -0300

    Fix comments on the PR

--- addons/popochiu/engine/objects/room/popochiu_room.gd
+++ addons/popochiu/engine/objects/room/popochiu_room.gd
@@ -131,19 +131,20 @@ func add_character(chr: PopochiuCharacter) -> void:
 	chr.started_walk_to.connect(_update_navigation_path)
 	chr.stopped_walk.connect(_clear_navigation_path.bind(chr))
 	
-	if chr == C.player:
-		chr.started_walk_to.connect(_follow_player)
+	if chr.follow_player:
+		C.player.started_walk_to.connect(_follow_player.bind(chr))
 
 	chr.idle()
 
 func _follow_player(
-	character: PopochiuCharacter, start_position: Vector2, end_position: Vector2
+	character: PopochiuCharacter, start_position: Vector2, end_position: Vector2, follower: PopochiuCharacter
 ):
-	await _update_navigation_path(character, start_position, end_position)
-	for c in get_characters():
-		var room_character = c as PopochiuCharacter
-		if not room_character.script_name == C.player.script_name && room_character.follow_player:
-			room_character.walk_to(end_position + room_character.follow_player_offset)
+	var follower_end_position
+	if end_position.x > follower.position.x:
+		follower_end_position = end_position - follower.follow_player_offset
+	else:
+		follower_end_position = end_position + follower.follow_player_offset
+	follower.walk_to(follower_end_position)
 
 func remove_character(chr: PopochiuCharacter) -> void:
 	$Characters.remove_child(chr)
