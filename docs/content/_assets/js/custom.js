473c40f70ed3b4e1f2f98808cd3b710837bea6fc
Paolo Pustorino <stickgrinder@gmail.com>
2024-02-17 19:33:13 +0100

    refs #147: GDScript codeblocks are now highlighted in documentation site. Added support for ancillary languages that may come in handy (md, bash, yaml, json, dockerfile, makefile).

--- docs/content/_assets/js/custom.js
+++ docs/content/_assets/js/custom.js
@@ -302,6 +302,9 @@ $(document).ready(() => {
     }
   }
 
+  // Activate Highlight.js syntax-highlighter
+  hljs.highlightAll();
+
   // Change indentation from spaces to tabs for codeblocks.
   const codeBlocks = document.querySelectorAll('.rst-content div[class^="highlight"] pre');
   for (const codeBlock of codeBlocks) {
