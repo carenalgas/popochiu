f1a230cf6eebfde4e7e0e26f40d776735ccb2ca2
Carenalga <mapedorr@gmail.com>
2024-04-23 05:44:36 -0500

    Fix #216 (#220)
    Fix #216 by using `queue_free()` to delete rows in a **PopochiuGroup**, use `free()` so the name of the node is available.

--- addons/popochiu/editor/main_dock/popochiu_group/popochiu_group.gd
+++ addons/popochiu/editor/main_dock/popochiu_group/popochiu_group.gd
@@ -1,47 +1,47 @@
 @tool
-@icon('res://addons/popochiu/editor/main_dock/popochiu_group/popochiu_group.svg')
+@icon("res://addons/popochiu/editor/main_dock/popochiu_group/popochiu_group.svg")
 class_name PopochiuGroup
 extends PanelContainer
 
 signal create_clicked
 
-const Constants := preload('res://addons/popochiu/popochiu_resources.gd')
+const Constants := preload("res://addons/popochiu/popochiu_resources.gd")
 const PopochiuObjectRow :=\
-preload('res://addons/popochiu/editor/main_dock/object_row/popochiu_object_row.gd')
+preload("res://addons/popochiu/editor/main_dock/object_row/popochiu_object_row.gd")
 
 @export var icon: Texture2D : set = _set_icon
 @export var is_open := true : set = _set_is_open
-@export var color: Color = Color('999999') : set = _set_color
-@export var title := 'Group' : set = _set_title
+@export var color: Color = Color("999999") : set = _set_color
+@export var title := "Group" : set = _set_title
 @export var can_create := true
-@export var create_text := ''
-@export var target_list: NodePath = ''
+@export var create_text := ""
+@export var target_list: NodePath = ""
 @export var custom_title_count := false
 
 var _external_list: VBoxContainer = null
 
-@onready var _header: PanelContainer = find_child('Header')
-@onready var _arrow: TextureRect = find_child('Arrow')
-@onready var _icon: TextureRect = find_child('Icon')
-@onready var _lbl_title: Label = find_child('Title')
-@onready var _body: Container = find_child('Body')
-@onready var _list: VBoxContainer = find_child('List')
-@onready var _btn_create: Button = find_child('BtnCreate')
+@onready var _header: PanelContainer = find_child("Header")
+@onready var _arrow: TextureRect = find_child("Arrow")
+@onready var _icon: TextureRect = find_child("Icon")
+@onready var _lbl_title: Label = find_child("Title")
+@onready var _body: Container = find_child("Body")
+@onready var _list: VBoxContainer = find_child("List")
+@onready var _btn_create: Button = find_child("BtnCreate")
 
 
-# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ GODOT ░░░░
+#region Godot ######################################################################################
 func _ready() -> void:
 	# Establecer estado inicial
 	add_theme_stylebox_override(
-		'panel', get_theme_stylebox('panel').duplicate()
+		"panel", get_theme_stylebox("panel").duplicate()
 	)
-	(get_theme_stylebox('panel') as StyleBoxFlat).border_color = color
+	(get_theme_stylebox("panel") as StyleBoxFlat).border_color = color
 	
 	if is_instance_valid(icon):
 		_icon.texture = icon
 	
 	_lbl_title.text = title
-	_btn_create.icon = get_theme_icon('Add', 'EditorIcons')
+	_btn_create.icon = get_theme_icon("Add", "EditorIcons")
 	_btn_create.text = create_text
 	self.is_open = _list.get_child_count() > 0
 	
@@ -50,17 +50,21 @@ func _ready() -> void:
 	
 	_header.gui_input.connect(_on_input)
 	_list.resized.connect(_update_child_count)
-	_btn_create.pressed.connect(emit_signal.bind('create_clicked'))
+	_btn_create.pressed.connect(emit_signal.bind("create_clicked"))
 	
 	if target_list:
 		_external_list = get_node(target_list) as VBoxContainer
 		self.is_open = _external_list.get_child_count() > 0
 
 
-# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ PUBLIC ░░░░
+#endregion
+
+#region Public #####################################################################################
 func clear_list() -> void:
 	for c in _list.get_children():
-		c.queue_free()
+		# Fix #216: Delete the row immediately so that it does not interfere with the creation of
+		# other rows that may have the same name as it
+		c.free()
 
 
 func add(node: Node, sort := false) -> void:
@@ -110,12 +114,14 @@ func add_header_button(btn: Button) -> void:
 
 func set_title_count(count: int, max_count := 0) -> void:
 	if max_count > 0:
-		_lbl_title.text = '%s (%d/%d)' % [title, count, max_count]
+		_lbl_title.text = "%s (%d/%d)" % [title, count, max_count]
 	else:
-		_lbl_title.text = '%s (%d)' % [title, count]
+		_lbl_title.text = "%s (%d)" % [title, count]
 
 
-# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ PRIVATE ░░░░
+#endregion
+
+#region Private ####################################################################################
 func _on_input(event: InputEvent) -> void:
 	var mouse_event: = event as InputEventMouseButton
 	if mouse_event and mouse_event.button_index == MOUSE_BUTTON_LEFT \
@@ -126,9 +132,10 @@ func _on_input(event: InputEvent) -> void:
 
 func _toggled(button_pressed: bool) -> void:
 	if is_instance_valid(_arrow):
-		_arrow.texture = get_theme_icon('GuiTreeArrowDown', 'EditorIcons')\
-			if button_pressed\
-			else get_theme_icon('GuiTreeArrowRight', 'EditorIcons')
+		_arrow.texture = (
+			get_theme_icon("GuiTreeArrowDown", "EditorIcons") if button_pressed
+			else get_theme_icon("GuiTreeArrowRight", "EditorIcons")
+		)
 	
 	if is_instance_valid(_body):
 		if button_pressed: _body.show()
@@ -142,7 +149,7 @@ func _set_color(value: Color) -> void:
 	color = value
 	
 	if is_instance_valid(_header):
-		(get_theme_stylebox('panel') as StyleBoxFlat).border_color = value
+		(get_theme_stylebox("panel") as StyleBoxFlat).border_color = value
 
 
 func _set_title(value: String) -> void:
@@ -170,7 +177,7 @@ func _update_child_count() -> void:
 	
 	if is_instance_valid(_lbl_title):
 		var childs := _list.get_child_count()
-		_lbl_title.text = title + (' (%d)' % childs) if childs > 1 else title
+		_lbl_title.text = title + (" (%d)" % childs) if childs > 1 else title
 
 
 func _order_list(node: Node) -> void:
@@ -186,3 +193,6 @@ func _order_list(node: Node) -> void:
 	if not place_before: return
 	
 	_list.move_child(node, place_before.get_index())
+
+
+#endregion
