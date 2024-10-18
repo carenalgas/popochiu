95f3668e8c2b4b9dfc079beb2f3252041b77dfa1
Anthony Irwin <anthony@anthonyirwin.com>
2024-10-18 22:56:26 +1000

    Update addons/popochiu/engine/objects/prop/popochiu_prop.gd
    Co-authored-by: Carenalga <mapedorr@gmail.com>

--- addons/popochiu/engine/objects/prop/popochiu_prop.gd
+++ addons/popochiu/engine/objects/prop/popochiu_prop.gd
@@ -206,7 +206,8 @@ func queue_pause_animation() -> Callable:
 
 
 ## Will pause the animation that is currently playing on the Popochiu Prop.
-## Call play_animation() without any parameter to resume the animation from the paused animation frame.
+## Will pause the animation that is currently playing.
+## Call [method play_animation] without any parameters to resume the animation.
 func pause_animation() -> void:
 	if not has_node("AnimationPlayer"): return
 	$AnimationPlayer.pause()
