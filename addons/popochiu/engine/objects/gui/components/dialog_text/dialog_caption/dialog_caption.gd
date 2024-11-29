44ecc0f8afc32fff676a8bb3d515e23f65358f80
balloonpopper <5151242+balloonpopper@users.noreply.github.com>
2024-11-30 10:28:28 +1100

    Update addons/popochiu/engine/objects/gui/components/dialog_text/dialog_caption/dialog_caption.gd
    Co-authored-by: Carenalga <mapedorr@gmail.com>

--- addons/popochiu/engine/objects/gui/components/dialog_text/dialog_caption/dialog_caption.gd
+++ addons/popochiu/engine/objects/gui/components/dialog_text/dialog_caption/dialog_caption.gd
@@ -19,7 +19,7 @@ func _modify_size(msg: String, target_position: Vector2) -> void:
 ## messy.
 func _append_text(msg: String, props: Dictionary) -> void:
 	rich_text_label.text = msg
-	var number_of_lines_of_text = rich_text_label.get_line_count()
+	var number_of_lines_of_text := rich_text_label.get_line_count()
 	if number_of_lines_of_text > 1:
 		var current_line_number = 0
 		for current_character in range(0, rich_text_label.text.length()):
