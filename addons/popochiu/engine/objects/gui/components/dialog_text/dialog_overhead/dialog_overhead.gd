85366a11d6528a1b6a63322f1a9a8fff2478f390
Balloonpopper <balloonpopper@git.com>
2024-12-05 21:15:42 +1100

    issue_360 overhead dialog now positioned correctly and line breaks inserted between lines

--- addons/popochiu/engine/objects/gui/components/dialog_text/dialog_overhead/dialog_overhead.gd
+++ addons/popochiu/engine/objects/gui/components/dialog_text/dialog_overhead/dialog_overhead.gd
@@ -4,7 +4,6 @@ extends PopochiuDialogText
 #region Private ####################################################################################
 func _modify_size(msg: String, target_position: Vector2) -> void:
 	var _size := await _calculate_size(msg)
-	
 	# Define size and position (before calculating overflow)
 	rich_text_label.size = _size
 	rich_text_label.position = target_position - rich_text_label.size / 2.0
@@ -27,13 +26,13 @@ func _set_default_label_size(lbl: Label) -> void:
 
 
 func _append_text(msg: String, props: Dictionary) -> void:
+	msg = _correct_line_breaks(msg)
+	
 	var center: float = floor(rich_text_label.position.x + (rich_text_label.size.x / 2))
 	if center == props.position.x:
 		rich_text_label.text = "[center]%s[/center]" % msg
 	elif center < props.position.x:
 		rich_text_label.text = "[right]%s[/right]" % msg
-	else:
-		rich_text_label.text = msg
 
 
 func _get_icon_from_position() -> float:
@@ -44,4 +43,30 @@ func _get_icon_to_position() -> float:
 	return rich_text_label.size.y / 2.0 + 3.0
 
 
+## Appends text for the dialog caption
+## Ensures that where a printing a word would see it wrap to the next line that the newline
+## is forced into the text. Without this the tween in dialog_text.gd would print part of the word
+## until it runs out of space, then erase the part word and rewrite it on the next line which looks
+## messy.
+func _correct_line_breaks(msg: String) -> String:
+	rich_text_label.text = msg
+	var number_of_lines_of_text := rich_text_label.get_line_count()
+	if number_of_lines_of_text > 1:
+		var current_line_number := 0
+		for current_character in range(0, rich_text_label.text.length()):
+
+			var ThisChar = rich_text_label.text[current_character]
+			var ThisLine = rich_text_label.get_character_line(current_character)
+			if rich_text_label.get_character_line(current_character) > current_line_number:
+				current_line_number += 1
+				if rich_text_label.text[current_character-1] == " ":
+					rich_text_label.text[current_character-1] = "\n"
+				elif rich_text_label.text[current_character-1] != "\n":
+					rich_text_label.text = rich_text_label.text.left(current_character) + "\n" + rich_text_label.text.right(-current_character)
+
+				if current_line_number == number_of_lines_of_text - 1:
+					break
+	return rich_text_label.text
+
+	
 #endregion
