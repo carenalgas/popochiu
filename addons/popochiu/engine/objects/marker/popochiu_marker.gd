38a2974d060a1a351a09c81802f36bbf0aa14285
Paolo Pustorino <stickgrinder@gmail.com>
2025-02-09 17:42:09 +0100

    refs #234: Saving WIP before switching branches for release.

---
+++ addons/popochiu/engine/objects/marker/popochiu_marker.gd
@@ -0,0 +1,13 @@
+@tool
+class_name PopochiuMarker
+extends Node
+## Exposes a Vector2 representing a position on the stage of a Room.
+##
+## It solves the same purpose as the [code]Marker2D[/code] node, but it is
+## shown by means of a Gizmo2D in the editor, for consistency with other
+## Popochiu objects.
+
+## The identifier of the object used in scripts.
+@export var script_name := ""
+## The [Vector2] position that the marker represents.
+@export var marker_point := Vector2.ZERO
