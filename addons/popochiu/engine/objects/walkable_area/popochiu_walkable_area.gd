0067beba591c95d2f5b55f86da954de2450c56f2
Paolo Pustorino <stickgrinder@gmail.com>
2025-08-06 23:13:49 +0200

    refs #131: Overall cleanup of the solution. There are still issues with the navigation area size.

--- addons/popochiu/engine/objects/walkable_area/popochiu_walkable_area.gd
+++ addons/popochiu/engine/objects/walkable_area/popochiu_walkable_area.gd
@@ -33,19 +33,18 @@ var map_rid: RID
 ## Used to assign a map in the [NavigationServer2D] to the region RID of the [b]$Perimeter[/b]
 ## child.
 var region_rid: RID
-# Add these properties to store RIDs
-var _obstacle_rids = []
 
 #region Godot ######################################################################################
 func _ready() -> void:
 	add_to_group('walkable_areas')
 
+	if not $Perimeter:
+		return
+
+	# We are in the editor so let's address what's needed to edit the perimenter polygon
 	if Engine.is_editor_hint():
-		# Ignore assigning the polygon when:
-		if (
-			get_node_or_null("Perimeter") == null # there is no NavigationArea2D node
-			or not get_parent() is Node2D # editing it in the .tscn file of the object directly
-		):
+		# Ignore assigning the polygon when editing it in the .tscn file of the object directly
+		if get_parent() is Node2D:
 			return
 
 		# Add interaction polygon to the proper group
@@ -53,7 +52,7 @@ func _ready() -> void:
 			PopochiuEditorHelper.POPOCHIU_OBJECT_POLYGON_GROUP
 		)
 
-		# Take the reference to the navigation polygon
+		# Get the reference to the navigation polygon
 		var navpoly: NavigationPolygon = $Perimeter.navigation_polygon
 		if interaction_polygon.is_empty():
 			# Save all the NavigationPolygon outlines in the local variable
@@ -62,19 +61,18 @@ func _ready() -> void:
 			# Save the NavigationRegion2D position
 			interaction_polygon_position = $Perimeter.position
 		else:
-			clear_and_bake(navpoly)
+			_load_navigation_polygon(navpoly)
+			_bake_navigation()
 
 		# If we are in the editor, we're done
 		return
 
 	# When the game is running...
-	# Update the node's polygon when:
-	if (
-		get_node_or_null("Perimeter") # there is an Perimeter node
-	):
-		# Take the reference to the navigation polygon
-		var navpoly: NavigationPolygon = $Perimeter.navigation_polygon
-		clear_and_bake(navpoly)
+	# Bake the perimeter polygon in the navigation server
+	# Take the reference to the navigation polygon
+	var navpoly: NavigationPolygon = $Perimeter.navigation_polygon
+	_load_navigation_polygon(navpoly)
+	_bake_navigation()
 
 	# Map the necessary resources
 	map_rid = NavigationServer2D.get_maps()[0]
@@ -84,7 +82,7 @@ func _ready() -> void:
 
 func _notification(event: int) -> void:
 	if event == NOTIFICATION_EDITOR_PRE_SAVE:
-		map_navigation_polygon($Perimeter)
+		_save_navigation_polygon($Perimeter)
 		# Saving the scene is necessary to make the changes permanent.
 		# If you remove this the character won't be able to walk in the area.
 		PopochiuEditorHelper.pack_scene(self)
@@ -99,9 +97,9 @@ func _exit_tree():
 #endregion
 
 #region Public #####################################################################################
-## Maps the outlines in [param perimeter] to the [member interaction_polygon] property and also
-## stores its position in [member interaction_polygon_position].
-func map_navigation_polygon(perimeter: NavigationRegion2D) -> void:
+# Maps the outlines in [param perimeter] to the [member interaction_polygon] property and also
+# stores its position in [member interaction_polygon_position].
+func _save_navigation_polygon(perimeter: NavigationRegion2D) -> void:
 	# Take the reference to the navigation polygon
 	var navpoly: NavigationPolygon = perimeter.navigation_polygon
 	
@@ -116,21 +114,38 @@ func map_navigation_polygon(perimeter: NavigationRegion2D) -> void:
 	interaction_polygon_position = perimeter.position
 
 
-## Populates [param navpoly] with all the outlines and bakes it back.
-func clear_and_bake(navpoly: NavigationPolygon) -> void:
+# Populates [param navpoly] with all the outlines and bakes it back.
+func _load_navigation_polygon(navpoly: NavigationPolygon) -> void:
 	if not navpoly:
 		return
 	
 	navpoly.clear_outlines()
 	for outline in interaction_polygon:
 		navpoly.add_outline(outline)
-	NavigationServer2D.bake_from_source_geometry_data(
-		navpoly, NavigationMeshSourceGeometryData2D.new()
-	)
 
 	# Restore the NavigationRegion2D position
 	$Perimeter.position = interaction_polygon_position
 
+
+func _bake_navigation(source_geometry: NavigationMeshSourceGeometryData2D = null) -> void:
+	# This is a convenience method to bake the navigation polygon
+	if not $Perimeter or not $Perimeter is NavigationRegion2D:
+		return
+	
+	var navpoly: NavigationPolygon = $Perimeter.navigation_polygon
+	if not navpoly:
+		return
+	
+	# If no source geometry is specified, use an empty one.
+	if not source_geometry:
+		source_geometry = NavigationMeshSourceGeometryData2D.new()
+	
+	# Now use the perimeter's navigation polygon for baking
+	NavigationServer2D.bake_from_source_geometry_data(navpoly, source_geometry)
+	# Make sure the region is up to date and linked to the map
+	NavigationServer2D.region_set_navigation_polygon(region_rid, navpoly)
+	# Force navigation update using the existing map relationship
+	NavigationServer2D.map_force_update(map_rid)
 
 
 
@@ -139,9 +154,7 @@ func clear_prop_obstacles() -> void:
 	var perimeter = get_node_or_null("Perimeter")
 	if not perimeter or not perimeter is NavigationRegion2D:
 		return
-		
-	print("Clearing navigation obstacles for area: %s" % name)
-	
+
 	# Create a fresh navigation polygon
 	var navpoly = NavigationPolygon.new()
 	
@@ -149,32 +162,13 @@ func clear_prop_obstacles() -> void:
 	for outline in interaction_polygon:
 		navpoly.add_outline(outline)
 	
-	# Bake without any obstacles
-	NavigationServer2D.bake_from_source_geometry_data(
-		navpoly, NavigationMeshSourceGeometryData2D.new()
-	)
-	
-	# Assign the polygon to the region
+	# Assign the polygon to the perimeter first - this establishes the relationship
 	perimeter.navigation_polygon = navpoly
-	
-	# Make sure the region is up to date
-	NavigationServer2D.region_set_map(perimeter.get_region_rid(), map_rid)
-	NavigationServer2D.region_set_navigation_polygon(perimeter.get_region_rid(), navpoly)
-	
+
+	_bake_navigation()
+
 	# Restore position
 	perimeter.position = interaction_polygon_position
-	
-	# Free any obstacle RIDs we've created
-	for obstacle_rid in _obstacle_rids:
-		if obstacle_rid.is_valid():
-			NavigationServer2D.free_rid(obstacle_rid)
-	
-	_obstacle_rids.clear()
-	
-	# Force navigation update
-	NavigationServer2D.map_force_update(map_rid)
-	
-	print("Navigation mesh restored to original state for area: %s" % name)
 
 
 ## Sets up navigation obstacles using projected obstructions.
@@ -183,24 +177,13 @@ func setup_prop_obstacles(obstacles: Array[NavigationObstacle2D]) -> void:
 	var perimeter = get_node_or_null("Perimeter")
 	if not perimeter or not perimeter is NavigationRegion2D:
 		return
-		
-	# Clear existing obstacles first
-	clear_prop_obstacles()
-	
-	print("Setting up %d obstacles for walkable area %s" % [obstacles.size(), name])
-	
-	# Create a NEW navigation polygon - important for ownership reasons
-	var navpoly = NavigationPolygon.new()
-	
-	# Add the original outlines to the new navigation polygon
-	for outline in interaction_polygon:
-		navpoly.add_outline(outline)
-		
+
+	_load_navigation_polygon(perimeter.navigation_polygon)
+
 	# Create source geometry data for baking
 	var source_geometry = NavigationMeshSourceGeometryData2D.new()
-	
+
 	# Add each obstacle as a projected obstruction BEFORE parsing
-	var any_obstacles_added = false
 	for obstacle in obstacles:
 		if not obstacle or obstacle.vertices.size() < 3:
 			continue
@@ -219,36 +202,11 @@ func setup_prop_obstacles(obstacles: Array[NavigationObstacle2D]) -> void:
 		
 		# Add as projected obstruction (true means carving)
 		source_geometry.add_projected_obstruction(local_vertices, true)
-		any_obstacles_added = true
-		print("Added projected obstruction with %d vertices at positions: %s" % [local_vertices.size(), local_vertices])
 	
-	# Only continue if we actually added obstacles
-	if not any_obstacles_added:
-		print("No valid obstacles to add")
-		return
-		
-	# Parse source geometry (which now includes obstacle projections)
-	NavigationServer2D.bake_from_source_geometry_data(navpoly, source_geometry)
-	
-	# Assign the new polygon to the region
-	perimeter.navigation_polygon = navpoly
-	
-	# Get the region_rid
-	region_rid = perimeter.get_region_rid()
-	
-	# Make sure the region is up to date and linked to the map
-	NavigationServer2D.region_set_map(region_rid, map_rid)
-	NavigationServer2D.region_set_navigation_polygon(region_rid, navpoly)
-	
-	# Force navigation update
-	NavigationServer2D.map_force_update(map_rid)
-	
+	_bake_navigation(source_geometry)
+
 	# Wait a frame to ensure navigation is properly updated
 	await get_tree().process_frame
-	
-	print("Navigation mesh rebaked with obstacles for area: %s" % name)
-	
-	return
 
 
 #endregion
