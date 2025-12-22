1937fbc56b770054cf73d50d9cab9d3d6a7315cf
Cidney Hamilton <cidney@cidneyhamilton.com>
2025-12-21 19:27:08 -0500

    Add documentation indicating this is a temporary workaround and providing context.

--- addons/popochiu/engine/audio_manager/audio_manager.gd
+++ addons/popochiu/engine/audio_manager/audio_manager.gd
@@ -280,6 +280,13 @@ func _play(
 	player.bus = cue.bus
 	player.play(from_position)
 
+	
+	# This is a workaround for a Godot 4.5 bug
+	# See https://github.com/godotengine/godot/commit/ccfbdf2bb2f109ba83533a45ef48dd0b219fc872 and https://github.com/godotengine/godot/commit/ccfbdf2bb2f109ba83533a45ef48dd0b219fc872 
+	# Fixes #463 Audio cues looping inappropriately in Godot 4.5
+	#
+	# TODO: remove this as soon as it becomes irrelevant
+	
 	if player.stream.get_class() == 'AudioStreamWAV': 
 		player.stream.loop_mode = (
 			AudioStreamWAV.LOOP_FORWARD if cue.loop else AudioStreamWAV.LOOP_DISABLED
