af1459e9199492e8348651261896d9386878c78a
David Briscoe <43559+idbrii@users.noreply.github.com>
2025-12-22 03:00:28 -0800

    refs #470: react to frame_changed from types different from Sprite2D for anti-glide animations
    * Recognize frame_changed from AnimatedSprite2D
    
    Fix anti_glide_animation doesn't work if you change a Character's
    Sprite2D to an AnimatedSprite2D.
    
    We only need to ensure the signal exists so we can connect to it, so
    check that instead of the type. This is only done on _ready, so it
    shouldn't be a perf problem.
    
    The Popochiu recommended method is to use a Sprite2D + AnimationPlayer,
    but this code is specifically looking for a signal so check what we need
    instead of what provides it.
    
    * refs #470: rephrased a comment to make it clearer to future us.
    
    ---------
    
    Co-authored-by: Paolo Pustorino <stickgrinder@gmail.com>

--- addons/popochiu/engine/objects/character/popochiu_character.gd
+++ addons/popochiu/engine/objects/character/popochiu_character.gd
@@ -206,7 +206,10 @@ func _ready():
 	# Connect the logic for anti-glide animations.
 	# The handler function will know what to do, based on configuration.
 	for child in get_children():
-		if not child is Sprite2D:
+		# Use the presence of the "frame_changed" signal instead of checking for a
+		# specific node type (would be Sprite2D). Improves resilience if the
+		# node structure gets altered, as long as the new node still emits the same signal.
+		if not child.has_signal("frame_changed"):
 			continue
 		child.frame_changed.connect(_update_position)
 
