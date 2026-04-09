af57d9aa3f42ddb4bf08713ee5d6212057092425
Paolo Pustorino <stickgrinder@gmail.com>
2026-04-09 14:01:18 +0200

    refs #133: high-res GUI docs added.

--- addons/popochiu/engine/objects/gui/templates/9_verb_high_res/9_verb_high_res_gui.gd
+++ addons/popochiu/engine/objects/gui/templates/9_verb_high_res/9_verb_high_res_gui.gd
@@ -1,1 +1,7 @@
+# @popochiu-docs-category game-user-interface
+class_name NineVerbHighResGUI
 extends NineVerbGUI
+## Defines the behavior of the 9 Verbs High-Res GUI.
+##
+## This GUI is functionally identical to [NineVerbGUI], but is designed for high-resolution
+## displays. All behavior is inherited from [NineVerbGUI].
