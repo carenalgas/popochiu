bff5d364bae8c1d066f52c5d39ecfde591c19e24
Paolo Pustorino <stickgrinder@gmail.com>
2026-08-20 12:35:29 +0200

    refs #558: Tab room has been converted to Tree control node as well.

--- addons/popochiu/editor/main_dock/tab_main/tab_main.gd
+++ addons/popochiu/editor/main_dock/tab_main/tab_main.gd
@@ -10,12 +10,6 @@ const ADD_TO_CORE_ICON = preload(
 )
 const PLAYER_ICON = preload("res://addons/popochiu/icons/player_character.png")
 const START_ICON = preload("res://addons/popochiu/icons/inventory_item_start.png")
-const AudioCue = preload("res://addons/popochiu/engine/audio_manager/audio_cue.gd")
-
-const DELETE_MESSAGE = "This will remove the [b]%s[/b] object in [b]%s[/b] scene. Uses of this \
-object in scripts will not work anymore. This action cannot be undone. Continue?"
-const DELETE_ASK_MESSAGE = "Do you want to delete the [b]%s[/b] folder too?%s (cannot be reversed)"
-const AUDIO_FILE_TYPES = ["AudioStreamOggVorbis", "AudioStreamMP3", "AudioStreamWAV"]
 
 enum MenuOptions {
 	SEPARATOR = -1,
@@ -36,7 +30,6 @@ enum Buttons {
 
 var _rows_paths := []
 var _has_data := false
-var _delete_dialog: PopochiuEditorHelper.DeleteConfirmation
 
 var _types := {
 	PopochiuResources.Types.ROOM: {
@@ -376,49 +369,6 @@ func _toggle_start_with_it(item: TreeItem) -> void:
 	data.is_on_start = is_on_start
 
 
-func _open(item: TreeItem) -> void:
-	# Defer the scene opening to ensure current operations complete first.
-	# Ugly but necessary to avoid errors (see _deferred_open comments).
-	call_deferred("_deferred_open", item.get_metadata(COL_TEXT).path)
-
-
-# Deferring this call removes an error that happens in Godot 4.4:
-# 'ERROR: editor/editor_data.cpp:1216 - Condition "!p_node->is_inside_tree()" is true.'
-func _deferred_open(path: String) -> void:
-	EditorInterface.select_file(path)
-	
-	if ".tres" in path:
-		EditorInterface.edit_resource(load(path))
-	else:
-		# Change to 2D view and open the scene
-		EditorInterface.set_main_screen_editor("2D")
-		EditorInterface.open_scene_from_path(path)
-		
-		# Select the scene root node automatically
-		# to make inspector, gizmos and toolbar available
-		# for the opened object scene
-		if EditorInterface.get_edited_scene_root():
-			PopochiuEditorHelper.select_node(EditorInterface.get_edited_scene_root())
-
-
-func _open_script(item: TreeItem) -> void:
-	var path: String = item.get_metadata(COL_TEXT).path
-	var script_path := path
-	
-	if ".tscn" in path:
-		# A room, character, inventory item, or prop
-		script_path = path.replace(".tscn", ".gd")
-	elif ".tres" in path:
-		# A dialog
-		script_path = path.replace(".tres", ".gd")
-	elif not ".gd" in path:
-		return
-	
-	EditorInterface.select_file(script_path)
-	EditorInterface.set_main_screen_editor("Script")
-	EditorInterface.edit_script(load(script_path))
-
-
 func _edit_state(item: TreeItem) -> void:
 	var path: String = item.get_metadata(COL_TEXT).path
 	EditorInterface.select_file(path.replace(".tscn", ".tres"))
@@ -473,30 +423,6 @@ func _add_object_to_core(item: TreeItem) -> void:
 	data.in_core = true
 
 
-func _remove_object(item: TreeItem) -> void:
-	var data := item.get_metadata(COL_TEXT)
-	var path: String = data.path
-	var name: String = item.get_text(COL_TEXT)
-	
-	# Look into the Object's folder for audio files and AudioCues to show the developer that those
-	# files will be removed too.
-	var audio_files := _search_audio_files(
-		EditorInterface.get_resource_filesystem().get_filesystem_path(path.get_base_dir())
-	)
-	
-	_delete_dialog = PopochiuEditorHelper.DELETE_CONFIRMATION_SCENE.instantiate()
-	_delete_dialog.title = "Remove %s from Popochiu" % name
-	_delete_dialog.message = DELETE_MESSAGE % [name, "Popochiu"]
-	_delete_dialog.ask = DELETE_ASK_MESSAGE % [
-		path.get_base_dir(),
-		"" if audio_files.is_empty()
-		else " ([b]%d[/b] audio cues will be deleted)" % audio_files.size()
-	]
-	_delete_dialog.on_confirmed = _remove_from_core.bind(item)
-	
-	PopochiuEditorHelper.show_delete_confirmation(_delete_dialog)
-
-
 func _remove_from_core(item: TreeItem, should_save_and_delete := true) -> void:
 	var data := item.get_metadata(COL_TEXT)
 	var type: int = data.type
@@ -537,161 +463,6 @@ func _remove_from_core(item: TreeItem, should_save_and_delete := true) -> void:
 	if should_save_and_delete:
 		EditorInterface.save_scene()
 		remove_item(item)
-
-
-# Remove this object's directory (subfolders included) from the file system.
-func _delete_from_file_system(path: String) -> void:
-	var object_dir: EditorFileSystemDirectory = \
-		EditorInterface.get_resource_filesystem().get_filesystem_path(path.get_base_dir())
-	
-	# Collect translatable file paths BEFORE deletion (directory object will be invalidated later)
-	var pot_paths_to_remove := PackedStringArray()
-	_collect_pot_paths(object_dir, pot_paths_to_remove)
-	
-	# Remove files, sub folders and its files.
-	_recursive_delete(object_dir)
-	
-	# Remove collected paths from the POT list AFTER deletion to avoid ProjectSettings.save()
-	# invalidating the EditorFileSystemDirectory reference used by _recursive_delete.
-	_deregister_pot_files(pot_paths_to_remove)
-
-
-# Removes the given [param paths_to_remove] from the POT generation list in a single batch save.
-func _deregister_pot_files(paths_to_remove: PackedStringArray) -> void:
-	if paths_to_remove.is_empty():
-		return
-	
-	var files := PopochiuConfig.get_pot_files()
-	var changed := false
-	for p in paths_to_remove:
-		var idx := files.find(p)
-		if idx >= 0:
-			files.remove_at(idx)
-			changed = true
-	if changed:
-		PopochiuConfig.sync_pot_files(files)
-
-
-# Recursively collects .gd and .tres file paths from [param dir] into [param result].
-func _collect_pot_paths(dir: EditorFileSystemDirectory, result: PackedStringArray) -> void:
-	for file_idx in dir.get_file_count():
-		var file_path := dir.get_file_path(file_idx)
-		var ext := file_path.get_extension()
-		if ext == "gd" or ext == "tres":
-			result.append(file_path)
-	for subdir_idx in dir.get_subdir_count():
-		_collect_pot_paths(dir.get_subdir(subdir_idx), result)
-
-
-# Remove the `dir` directory from the system. For Godot to be able to delete a directory, it has to
-# be empty, so this method first deletes the files from from the directory and each of its
-# subdirectories.
-func _recursive_delete(dir: EditorFileSystemDirectory) -> void:
-	if dir.get_file_count() > 0:
-		assert(
-			_delete_files(dir) == OK,
-			"[Popochiu] Error removing files in recursive elimination of %s" % dir.get_path()
-		)
-	
-	if dir.get_subdir_count() > 0:
-		for folder_idx in dir.get_subdir_count():
-			# Check if there are more folders inside the folder or delete the files inside it before
-			# deleting the folder itself
-			_recursive_delete(dir.get_subdir(folder_idx))
-	
-	assert(
-		DirAccess.remove_absolute(dir.get_path()) == OK,
-		"[Popochiu] Error removing folder in recursive elimination of %s" % dir.get_path()
-	)
-	EditorInterface.get_resource_filesystem().scan()
-
-
-# Delete files within [param dir] directory. First, get the paths to each file, then delete them
-# one by one calling [method EditorFileSystem.update_file], so that in case it's an imported file,
-# its [b].import[/b] is also deleted.
-func _delete_files(dir: EditorFileSystemDirectory) -> int:
-	# Stores the paths of the files to be deleted.
-	var files_paths := []
-	# Stores the paths of the audio resources to delete
-	var deleted_audios := []
-	
-	for file_idx: int in dir.get_file_count():
-		match dir.get_file_type(file_idx):
-			AUDIO_FILE_TYPES:
-				deleted_audios.append(dir.get_file_path(file_idx))
-			"Resource":
-				var resource: Resource = load(dir.get_file_path(file_idx))
-				if not resource is AudioCue:
-					# If the resource is not an AudioCue, then it should be ignored for deletion
-					# in the game data
-					continue
-				
-				# Delete the [PopochiuAudioCue] in the project data file and the A singleton
-				assert(
-					_delete_audio_cue_in_data(resource) == true,
-					"[Popochiu] Couldn't remove [b]%s[/b] during deletion of [b]%s[/b]." %
-					[resource.resource_path, dir.get_path()]
-				)
-				
-				deleted_audios.append(resource.audio.resource_path)
-		
-		files_paths.append(dir.get_file_path(file_idx))
-	
-	for fp: String in files_paths:
-		var err: int = DirAccess.remove_absolute(fp)
-		if err != OK:
-			PopochiuUtils.print_error("Couldn't delete file %s. err_code:%d" % [err, fp])
-			return err
-		EditorInterface.get_resource_filesystem().scan()
-	
-	# Delete the rows of audio files and the deleted AudioCues in the Audio tab
-	if not deleted_audios.is_empty():
-		PopochiuEditorHelper.signal_bus.audio_cues_deleted.emit(deleted_audios)
-	
-	# Remove extra files (like .import)
-	for file_name: String in DirAccess.get_files_at(dir.get_path()):
-		DirAccess.remove_absolute(dir.get_path() + "/" + file_name)
-		EditorInterface.get_resource_filesystem().scan()
-	
-	return OK
-
-
-func _search_audio_files(dir: EditorFileSystemDirectory) -> Array:
-	var files := []
-	
-	for idx in dir.get_subdir_count():
-		files.append_array(_search_audio_files(dir.get_subdir(idx)))
-	
-	for idx in dir.get_file_count():
-		match dir.get_file_type(idx):
-			AUDIO_FILE_TYPES:
-				files.append(dir.get_file_path(idx))
-	
-	return files
-
-
-# Looks to which audio group corresponds [param audio_cue] and deletes it both from
-# [code]popochiu_data.cfg[/code] and the [b]A[/b] singleton (which is the one used to allow code
-# autocompletion related to [PopochiuAudioCue]s).
-func _delete_audio_cue_in_data(audio_cue: AudioCue) -> bool:
-	# TODO: This could be improved a lot if each PopochiuAudioCue has a variable to store the group
-	# 		to which it corresponds to.
-	# Delete the [PopochiuAudioCue] in the popochiu_data.cfg
-	for cue_group in ["mx_cues", "sfx_cues", "vo_cues", "ui_cues"]:
-		var cues: Array = PopochiuResources.get_data_value("audio", cue_group, [])
-		if not audio_cue.resource_path in cues:
-			continue
-		
-		cues.erase(audio_cue.resource_path)
-		if PopochiuResources.set_data_value("audio", cue_group, cues) != OK:
-			return false
-		
-		# Fix #59 : remove the AudioCue from the A singleton
-		PopochiuResources.remove_audio_autoload(
-			cue_group, audio_cue.resource_name, audio_cue.resource_path
-		)
-		break
-	return true
 
 
 #endregion
